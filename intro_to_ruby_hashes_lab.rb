cea9e99d6ad34b73ee4e27f5c09873a53e169521
David Holmes <phoenix1988@gmail.com>
2019-12-23 13:23:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,10 +24,13 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash = {key}
   return hash[:key]
 end
 
 def update_counting_hash(hash, key)
-
+  if hash[key]
+  hash[key] += 1
+else
+  hash[key] = 1
+end
 end
